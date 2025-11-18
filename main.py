cafed62ad33045a6b2ca63779b8cd07b9903ec90
Artur Drzazga <artur.drzazga03@gmail.com>
2025-11-18 20:27:42 +0100

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import json
+
+from rest_framework import serializers
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(instance=car)
+    json_string = json.dumps(serializer.data)
+    return json_string.encode("utf-8")
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    file = json.decode("utf-8")
+    dict_data = json.loads(file)
+
+    serializer = CarSerializer(data=dict_data)
+    if serializer.is_valid():
+        car_instance = serializer.save()
+        return car_instance
+    else:
+        raise serializers.ValidationError(serializer.errors)
