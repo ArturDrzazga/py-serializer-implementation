f217d48ba7619de123768731738a1954b9f85af1
Artur Drzazga <artur.drzazga03@gmail.com>
2025-11-18 20:30:09 +0100

    fixed flake8

--- car/serializers.py
+++ car/serializers.py
@@ -19,22 +19,22 @@ class CarSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.manufacturer = validated_data.get(
-            'manufacturer',
+            "manufacturer",
             instance.manufacturer
         )
         instance.model = validated_data.get(
-            'model',
+            "model",
             instance.model
         )
         instance.horse_powers = validated_data.get(
-            'horse_powers',
+            "horse_powers",
             instance.horse_powers)
         instance.is_broken = validated_data.get(
-            'is_broken',
+            "is_broken",
             instance.is_broken
         )
         instance.problem_description = validated_data.get(
-            'problem_description',
+            "problem_description",
             instance.problem_description
         )
 
